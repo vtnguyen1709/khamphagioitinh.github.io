394bd8894fceb0e9aa3d31e0dd90aff31137d245
vtnguyen1709 <vtnguyen1709@gmail.com>
2023-11-23 06:25:51 +0700

    khao sat suc khoe doi nhom

--- script.js
+++ script.js
@@ -1,69 +1,96 @@
 var countQues=0;
 const CHOICES = [
-    ["Tôi rất tự tin, cả trong công việc và cuộc sống", "Tôi là người rất coi trọng tính kỷ luật", "Tôi luôn chú ý đến những gì người khác nói", "Tôi có xu hướng tin tưởng mọi người"],
-    ["Tôi là người có tính tự phát", "Tôi luôn cần nhiều lời khuyên trước khi đưa ra quyết định", "Tôi thuộc tuýp người thích mơ mộng", "Tôi giỏi phân tích tình huống"],
-    ["Tôi rất năng nổ tham gia các hoạt động của công ty", "Tôi luôn nằm trong vị trí ban tổ chức các chương trình của công ty ", "Tôi thường thẳng thắn trao đổi vấn đề 1 cách trực tiếp", "Tôi là tuýp người thích khám phá"],
-    ["Tôi khá bướng bỉnh", "Tôi không thích lắng nghe ý kiến của người khác", "Tôi thuộc tuýp người thích sự nổi loạn", "Tôi dễ bị xúc phạm"],
-    ["Tôi luôn yêu cầu và đòi hỏi ở mọi người rất nhiều", "Tôi thích được bảo bọc", "Dù biết mình sai nhưng tôi rất khó để chấp nhận", "Tôi thích sự yên lặng"],
-    ["Tôi thuộc tuýp người hoạt náo", "Mọi người bảo tôi luôn tiên phong trong ý tưởng", "Tôi không thích sự thay đổi ", "Tôi luôn có trách nhiệm với mọi việc"],
-    ["Tôi luôn thận trọng với mọi việc", "Tôi hay có những hành vi và phản ứng thái quá", "Tôi yêu thích sự hài hoà", "Mọi người đánh giá tôi là người rất nghị lực"],
-    ["Tôi luôn giúp đỡ mọi người", "Tôi luôn thẳng thắn đưa ra quan điểm của mình", "Tôi luôn kiên định với những sự lựa chọn của mình", "Tôi thuộc tuýp người điềm đạm và không nóng nảy"],
-    ["Tôi đáng tin cậy", "Mọi người nói tôi có cá tính mạnh mẽ", " Hành vi của tôi rất   kỷ luật", "Tôi là người có tính sở hữu cao"],
-    ["Dám nghĩ dám làm", "Duy tâm", "Nghiêm túc", "Vui tươi"],
-    ["Tôi luôn sắp xếp công việc 1 cách logic", "Tôi luôn biết hài lòng với hiện tại", "Tôi luôn thân thiện với mọi người", "Tôi rất táo bạo, dám nghĩ, dám làm"],
-    ["Tôi rất chăm chỉ", "Tôi thuộc tuýp người hay tưởng tượng", "Độ chính xác rất quan trong đối với tôi", "Tôi được mọi người yêu thích"],
-    ["Tôi luôn dè dặt với mọi người", "Mọi người bảo tôi luôn tiên phong trong ý tưởng", "Dù tôi làm gì, mọi người cũng sẽ khó rời mắt khỏi tôi", "Tôi luôn lạc quan"],
-    ["Tôi là người rất quyết đoán trong mọi vấn đề", "Tôi sẽ phát triển tốt khi làm việc nhóm", "Tôi sẽ phát triển tốt khi hoạt động độc lập", "Tôi thuộc tuýp người truyền thống"],
-    ["Tôi thường được khen vì ăn nói có duyên", "Tôi luôn bất an với mọi việc", "Tôi luôn đặt “chữ tâm” trong những việc tôi làm", "Mọi người thường nói tôi khiêm tốn "],
-    ["Lãnh đạo", "Cố vấn", "Thiết kế", "Quản lý"],
-    ["Tôi rất tỉ mỉ trong mọi việc", "Nghiện công việc", "Tôi luôn sẵn lòng giúp đỡ", "Tôi luôn đặt ra mục tiêu cụ thể trước khi bắt đầu"],
-    ["Cần cù là đặc điểm nổi trội của tôi", "Tôi là người rất chi tiết", "Tôi có óc tưởng tượng phong phú", "Tôi có thái độ rất tích cực"],
-    ["Tôi luôn có định hướng công việc rõ ràng", "Mọi việc tôi làm luôn lấy yếu tố con người làm trọng tâm", "Tôi luôn định hướng ý tưởng rõ ràng", "Tôi là người hướng đến kết quả"],
-    ["Tôi sống và làm việc theo cảm xúc", "Tôi thích ứng nhanh với mọi thay đổi", "Tôi luôn thu hút mọi ánh nhìn khi xuất hiện", "Tôi rất tỉ mỉ trong mọi việc"],
-    ["Tôi hay cáu kỉnh và nóng nảy", "Tôi khá cứng nhắc trong mọi việc", "Tôi dễ bị coi thường", "Tôi dễ bị đe dọa"],
-    ["Gián tiếp", "Tôi rất thẳng thắn", "Tôi rất cẩn thận", "Tôi thuộc tuýp người nghiêm khắc"],
-    ["Tôi luôn đặt ra mục tiêu cụ thể trước khi bắt đầu", "Tôi luôn có trách nhiệm với những việc tôi làm", "Tôi luôn sẵn sàng giúp đỡ mọi người", "Khi làm việc tôi luôn dành thời gian để lên kế hoạch cụ thể"],
-    ["Tôi luôn cố gắng làm mọi thứ để vượt qua khó khăn", "Tôi thích được khen ngợi", "Tôi là người khá hài hước ", "Tôi là người không thích bị người khác chú ý"],
-    ["Tôi là người thích quan sát", "Mọi người hay nói về tôi là người có ý chí", "Tôi luôn hăng hái, tích cực trong các hoạt động", "Tôi thường tỉ mỉ đến mức cầu toàn"],
-    ["Tôi rất tự tin", "Tôi có xu hướng là 1 người thận trọng", "Tôi thấu đáo", "Tôi rất thời thượng"],
-    ["Tôi thích mọi thứ gọn gàng và ngăn nắp", "Tôi rất quan tâm đến ngoại hình", "Tôi luôn muốn “đứng ngoài” những cuộc xung đột", "Tôi là người khá bảo thủ"]
+    "Các thành viên trong nhóm thừa nhận sai lầm của họ.",
+    "Các thành viên trong nhóm hăng hái và cởi mở khi thảo luận về các vấn đề cần giải quyết.",
+    "Các thành viên trong nhóm nhanh chóng nhận ra những đóng góp và thành tích của những người khác.",
+    "Trong các cuộc họp nhóm, những vấn đề quan trọng nhất và khó khăn nhất được mang ra thảo luận.",
+    "Các thành viên trong nhóm thừa nhận những điểm yếu của họ với nhau.",
+    "Các thành viên trong nhóm dám bày tỏ ý kiến của mình ngay cả khi có nguy cơ gây ra bất đồng ý kiến.",
+    "Các thành viên trong nhóm không ngại chỉ ra những hành vi cản trở sự phát triển của nhóm đến từ những thành viên khác. ",
+    "Nhóm bạn thường xuyên đạt kết quả tích cực trong công việc & đạt sự tin cậy của mọi người trong việc đạt kết quả cao ",
+    "Các thành viên trong nhóm yêu cầu được giúp đỡ mà không do dự.",
+    "Các thành viên trong nhóm rời khỏi cuộc họp với sự tự tin rằng mọi người đều cam kết thực hiện các quyết định đã được thống nhất.",
+    "Trong quá trình thảo luận, các thành viên trong nhóm thẳng thắng hỏi & làm rõ về cách các thành viên khác đưa ra kết luận và ý kiến của họ.",
+    "Các thành viên trong nhóm sẵn sàng hỏi & tham khảo ý kiến từ các thành viên khác.",
+    "Khi nhóm không đạt được mục tiêu chung, mỗi thành viên đều nhận trách nhiệm để cải thiện hiệu suất của nhóm.",
+    "Các thành viên trong nhóm sẵn sàng hy sinh lợi ích cá nhân của mình vì lợi ích chung của nhóm.",
+    "Các thành viên trong nhóm thẳng thắn hỏi đồng nghiệp  về các vấn đề thuộc lĩnh vực chịu trách nhiệm của họ.",
+    "Các thành viên trong nhóm đánh giá cao, học hỏi và tận dụng các kỹ năng và chuyên môn của nhau.",
+    "Các thành viên trong nhóm tham khảo ý kiến của nhau trong các cuộc họp.",
+    "Các thành viên trong nhóm kết thúc thảo luận với các giải pháp và KH hành động rõ ràng, cụ thể.",
+    "Các thành viên trong nhóm sẵn sàng xin lỗi nhau.",
+    "Các thành viên trong nhóm sẵn sàng chia sẻ những ý tưởng khác biệt cho nhóm.",
+    "Nhóm có định hướng rõ ràng & ưu tiên.",
+    "Các thành viên trong nhóm khiêm tốn về những đóng góp của họ cho nhóm.",
+    "Khi xung đột xảy ra, nhóm không né tránh mà tập trung giải quyết vấn đề trước khi chuyển sang chủ đề khác.",
+    "Nhóm cùng tập trung vào việc đạt được các mục tiêu chung. ",
+    "Nhóm rất quyết đoán, ngay cả khi không có tất cả đầy đủ thông tin. ",
+    "Các thành viên trong nhóm cởi mở và chân thành với nhau.",
+    "Các thành viên trong nhóm thoải mái chia sẻ về cuộc sống cá nhân của họ với nhau.",
+    "Nhóm luôn theo sát các quyết định của mình.",
+    "Các thành viên trong nhóm sẵn sàng đưa ra phản hồi mang tính xây dựng cho nhau.",
+    "Các thành viên trong nhóm ủng hộ các quyết định của nhóm ngay cả khi họ ban đầu không đồng ý.",
+    "Các thành viên trong nhóm tích cực theo dõi & giám sát hiệu quả chung của đội nhóm",
+    "Các thành viên trong nhóm sẵn sàng đặt câu hỏi về mức độ cam kết / sự nhận trách nhiệm của thành viên khác ",
+    "Các thành viên trong nhóm có cảm thấy chịu trách nhiệm về kết quả chung của nhóm",
+    "Tiến độ dự án có được thường xuyên xem xét & thảo luận",
+    "Nhóm trưởng/ các thành viên trong nhóm khuyến khich/ ghi nhận việc phản hồi cởi mở và trao đổi thắng thắn ",
+    "Nếu thấy có nguy cơ xảy ra sự cố và không đạt điều mình đã cam kết, các thành viên trong nhóm loan tin đến những người xunh quanh và đưa ra giải pháp để khắc phục",
+    "Nhóm trưởng & các thành viên trong nhóm khuyến khích mọi người chủ động nhận trách nhiệm khi các vấn đề phát sinh thay vì đổ lỗi cho hoàn cảnh/ bên ngoài",
+    "Nhóm có tổ chức các cuộc họp định kỳ để cập nhật tiến độ và kết quả công việc với tất cả thành viên",
+    "Mục tiêu chung của nhóm được xác định rõ ràng và là tiêu chí hàng đầu khi đánh giá hiệu quả/hiệu suất cá nhân",
+    "Khi mục tiêu chung của nhóm không được đạt, nhóm xác định rõ ràng nguyên nhân và đề ra kế hoạch hành động cụ thể để cải thiện "
 ];
 
 const RESULTS = [
-    ["red", "green", "blue", "yellow"],
-    ["red", "yellow", "blue", "green"],
-    ["yellow", "green", "red", "blue"],
-    ["green", "red", "yellow", "blue"],
-    ["red", "yellow", "green", "blue"],
-    ["yellow", "blue", "green", "red"],
-    ["green", "blue", "yellow", "red"],
-    ["yellow", "red", "green", "blue"],
-    ["yellow", "red", "green", "blue"],
-    ["red", "blue", "green", "yellow"],
-    ["green", "blue", "yellow", "red"],
-    ["red", "blue", "green", "yellow"],
-    ["green", "blue", "red", "yellow"],
-    ["red", "yellow", "blue", "green"],
-    ["yellow", "red", "green", "blue"],
-    ["red", "yellow", "blue", "green"],
-    ["green", "red", "yellow", "blue"],
-    ["red", "green", "blue", "yellow"],
-    ["green", "yellow", "blue", "red"],
-    ["blue", "yellow", "red", "green"],
-    ["red", "green", "blue", "yellow"],
-    ["yellow", "red", "blue", "green"],
-    ["red", "blue", "yellow", "green"],
-    ["red", "yellow", "blue", "green"],
-    ["blue", "red", "yellow", "green"],
-    ["red", "blue", "green", "yellow"],
-    ["green", "yellow", "blue", "red"],
+    "Sự tin tưởng",
+    "Xung đột",
+    "Kết quả",
+    "Xung đột",
+    "Sự tin tưởng",
+    "Xung đột",
+    "Trách nhiệm",
+    "Kết quả",
+    "Sự tin tưởng",
+    "Cam kết",
+    "Xung đột",
+    "Sự tin tưởng",
+    "Trách nhiệm",
+    "Kết quả",
+    "Trách nhiệm",
+    "Sự tin tưởng",
+    "Xung đột",
+    "Cam kết",
+    "Sự tin tưởng",
+    "Xung đột",
+    "Cam kết",
+    "Kết quả",
+    "Xung đột",
+    "Cam kết",
+    "Cam kết",
+    "Sự tin tưởng",
+    "Sự tin tưởng",
+    "Cam kết",
+    "Trách nhiệm",
+    "Cam kết",
+    "Trách nhiệm",
+    "Trách nhiệm",
+    "Trách nhiệm",
+    "Kết quả",
+    "Xung đột",
+    "Cam kết",
+    "Trách nhiệm",
+    "Kết quả",
+    "Kết quả",
+    "Kết quả"
 ];
 
 let SCORES = {
-    "red" : 0,
-    "green" : 0,
-    "blue": 0,
-    "yellow": 0
+    "Sự tin tưởng": 0,
+    "Xung đột" : 0,
+    "Kết quả" : 0,
+    "Cam kết": 0,
+    "Trách nhiệm": 0,
 };
 
 //alert(questions.length);
@@ -71,17 +98,16 @@ document.querySelector(".view-results").style.display="none";
 document.querySelector(".quiz").style.display="none";
 document.querySelector(".final-result").style.display="none";
 
-
 document.querySelector(".start-btn").addEventListener("click",function(){
     document.getElementById('wrapper').style.display = 'none';
 
 
     document.getElementById("ques-left").textContent="Câu hỏi : " + (countQues+1) + "/" + CHOICES.length;
     document.querySelector(".quiz").style.display="block";
-    document.querySelector(".question").innerHTML = "<h1> Chọn tính cách mà bạn thấy giống với bản thân nhất: </h1>";
-    for (let i = 0; i <= 3; i++) {
-        document.getElementById("lb" + i).innerHTML = CHOICES[countQues][i];
-    }
+    document.querySelector(".question").innerHTML = "<h1> " + CHOICES[countQues] + " </h1>";
+    // for (let i = 0; i <= 3; i++) {
+    //     document.getElementById("lb" + i).innerHTML = CHOICES[countQues][i];
+    // }
     document.querySelector('input[name="options"]:checked').checked = false
 });
 
@@ -92,9 +118,9 @@ document.querySelector(".submit-answer").addEventListener("click",function(){
         alert('Vui lòng chọn 1 đáp án!');
     }
     let selected = answerEl.value;
-
-    let result = RESULTS[countQues][selected];
-    SCORES[result] = SCORES[result] + 1;
+    if (selected === '0') {
+        SCORES[RESULTS[countQues]] += 1;
+    }
 
     if (countQues < CHOICES.length - 1) {
     // if (countQues < 2 - 1) {
@@ -106,9 +132,10 @@ document.querySelector(".submit-answer").addEventListener("click",function(){
     }
 
     document.getElementById("ques-left").textContent = "Câu hỏi : " + (countQues + 1) + "/" + CHOICES.length;
-    for (let i = 0; i <= 3; i++) {
-        document.getElementById("lb" + i).innerHTML = CHOICES[countQues][i];
-    }
+    document.querySelector(".question").innerHTML = "<h1> " + CHOICES[countQues] + " </h1>";
+    // for (let i = 0; i <= 3; i++) {
+    //     document.getElementById("lb" + i).innerHTML = CHOICES[countQues][i];
+    // }
 
     document.querySelector('input[name="options"]:checked').checked = false
 });
@@ -130,16 +157,14 @@ function iOS() {
 document.querySelector(".view-results").addEventListener("click",function(){
     document.querySelector(".quiz").style.display = "none";
     if (iOS() || (navigator.userAgentData && navigator.userAgentData.mobile)) {
-        let chartContainer = document.getElementById('chartContainer');
-        chartContainer.style.height = '150px';
-        chartContainer.style.marginTop = '200px';
-        chartContainer.style.maxWidth = "300px";
-        chartContainer.style.marginLeft = "-65px";
+        // let chartContainer = document.getElementById('chartContainer');
+        // chartContainer.style.height = '150px';
+        // chartContainer.style.marginTop = '200px';
+        // chartContainer.style.maxWidth = "300px";
+        // chartContainer.style.marginLeft = "-65px";
 
-        document.querySelector('.note').style.width = "100%";
 
     }
-
 
     document.querySelector(".final-result").style.display="block";
     renderResult();
@@ -147,30 +172,40 @@ document.querySelector(".view-results").addEventListener("click",function(){
 });
 
 function renderResult() {
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        title: {
-            text: ""
-        },
-        data: [{
-            type: "pie",
-            startAngle: 120,
-            yValueFormatString: "##",
-            indexLabel: "{label}",
-            dataPoints: [
-                // {y: SCORES["red"], label: "Red", color:"#FF0000"},
-                // {y: SCORES["yellow"], label: "Yellow", color: "#FFFF00"},
-                // {y: SCORES["blue"], label: "Blue", color: "#0000FF"},
-                // {y: SCORES["green"], label: "Green", color: "#008000"}
-                {y: SCORES["red"], label: "Dominance", color:"#EA2E25"},
-                {y: SCORES["yellow"], label: "Influence", color: "#FEC106"},
-                {y: SCORES["blue"], label: "Compliance", color: "#0071C1"},
-                {y: SCORES["green"], label: "Steadiness", color: "#008001"}
-            ]
+    var data = {
+        labels: ["Sự tin tưởng", "Xung đột", "Kết quả", "Cam kết", "Trách nhiệm"],
+        datasets: [{
+            label: 'Nhóm',
+            fill: true,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 2,
+            data: [SCORES["Sự tin tưởng"], SCORES["Xung đột"], SCORES["Kết quả"], SCORES["Cam kết"], SCORES["Trách nhiệm"]],
         }]
+    };
 
+    // Configuration options for the radar chart
+    var options = {
+        scales: {
+            r: {
+                angleLines: {
+                    display: false
+                },
+                suggestedMin: 0,
+                suggestedMax: 8
+            }
+        }
+    };
+
+    // Get the canvas element
+    var ctx = document.getElementById('canvas').getContext('2d');
+
+    // Create the radar chart
+    var radarChart = new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: options
     });
-    chart.render();
 }
 
 document.getElementById("restart").addEventListener("click",function(){
